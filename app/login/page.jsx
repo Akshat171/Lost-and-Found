36c4db78561b7377917da4f33729220b9c70669c
Akshat <akshatjangir1710@gmail.com>
2024-02-25 01:14:06 +0530

    change signup and login page UI

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -43,81 +43,122 @@ export default function LoginPage() {
 
   return (
     <>
-      <div className="container px-6 mx-auto">
-        <div className="flex flex-col text-center md:text-left md:flex-row h-screen justify-evenly md:items-center">
-          <div className="flex flex-col w-full">
-            <div>
-              <Image src={Icon} width={100} height={100} alt="icon" />
-            </div>
-            <h1 className="text-5xl text-gray-800 font-bold xs:text-xs">
+      <div className="h-screen flex">
+        <div
+          className="hidden lg:flex w-full lg:w-1/2 login_img_section
+          justify-around items-center"
+        >
+          <div
+            className=" 
+                  bg-black 
+                  opacity-20 
+                  inset-0 
+                  z-0"
+          ></div>
+          <div className="w-full mx-auto px-20 flex-col items-center space-y-6">
+            <h1 className="text-white font-bold text-4xl font-sans">
+              <Image
+                src={Icon}
+                width={50}
+                height={50}
+                alt="icon"
+                className="bg-white rounded-full"
+              />
               Campus Lost OR Found
             </h1>
-            <p className="w-8/12 pt-3 mx-auto md:mx-0 text-gray-500">
+            <p className="text-white mt-0.5 text-base">
               Lost something? Don't lose hope! We connect finders with owners.
             </p>
-          </div>
-          <div className="w-full md:w-full lg:w-9/12 mx-auto md:mx-0">
-            <div className="bg-white p-7 flex flex-col w-full shadow-xl rounded-xl items-center">
-              <h2 className="text-3xl font-bold text-gray-800 text-left mb-5">
-                Login
-              </h2>
-              <form action="" className="w-full">
-                <div id="input" className="flex flex-col w-full my-2">
-                  <label for="username" className="text-gray-500 mb-2">
-                    Email
-                  </label>
-                  <input
-                    id="email"
-                    type="email"
-                    autoComplete="off"
-                    value={user.email}
-                    onChange={(e) =>
-                      setUser({ ...user, email: e.target.value })
-                    }
-                    placeholder="email"
-                    className="appearance-none border-2 text-black border-gray-100 rounded-lg px-4 py-3 placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:shadow-lg"
-                  />
-                </div>
-                <div id="input" className="flex flex-col w-full my-2">
-                  <label for="password" className="text-gray-500 mb-2">
-                    Password
-                  </label>
-                  <input
-                    id="password"
-                    type="password"
-                    autoComplete="off"
-                    value={user.password}
-                    onChange={(e) =>
-                      setUser({ ...user, password: e.target.value })
-                    }
-                    placeholder="password"
-                    className="appearance-none border-2 text-black border-gray-100 rounded-lg px-4 py-3 placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:shadow-lg"
-                  />
-                </div>
-                <div id="button" className="flex flex-col w-full my-5">
-                  <button
-                    type="button"
-                    onClick={onLogin}
-                    className="w-full py-4 bg-green-600 rounded-lg text-green-100 hover:bg-green-800"
-                  >
-                    <div className="flex flex-row items-center justify-center">
-                      <div className="font-bold">Login</div>
-                      <div className="mr-2 text-xl">
-                        <BiLogIn />
-                      </div>
-                    </div>
-                  </button>
-                  <div className="flex justify-evenly mt-5">
-                    <Link
-                      href="/signup"
-                      className="w-full text-center font-medium text-gray-500 "
-                    >
-                      Visit Signup page
-                    </Link>
-                  </div>
-                </div>
-              </form>
+            {/*<div className="flex justify-center lg:justify-start mt-6">
+              <Link
+                href="/"
+                className="hover:bg-indigo-700 hover:text-white hover:-translate-y-1 transition-all duration-500 bg-white text-indigo-800 mt-4 px-4 py-2 rounded-2xl font-bold mb-2"
+              >
+                Get Started
+              </Link>
             </div>
+          */}
+          </div>
+        </div>
+        <div className="flex w-full lg:w-1/2 justify-center items-center bg-white space-y-8">
+          <div className="w-full px-8 md:px-32 lg:px-24">
+            <form className="bg-white rounded-md shadow-2xl p-5 text-black">
+              <h1 className="text-gray-800 font-bold text-2xl mb-1">
+                Welcome Back!
+              </h1>
+              <p className="text-sm font-normal text-gray-600 mb-8">
+                Please enter details here.
+              </p>
+              <div className="flex items-center border-2 mb-8 py-2 px-3 rounded-2xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-gray-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                  />
+                </svg>
+                <input
+                  id="email"
+                  className=" pl-2 text-black w-full outline-none border-none"
+                  type="email"
+                  autoComplete="off"
+                  value={user.email}
+                  onChange={(e) => setUser({ ...user, email: e.target.value })}
+                  placeholder="email"
+                />
+              </div>
+              <div className="flex items-center border-2 mb-12 py-2 px-3 rounded-2xl ">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-gray-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <input
+                  className="pl-2 text-black w-full outline-none border-none"
+                  id="password"
+                  type="password"
+                  autoComplete="off"
+                  value={user.password}
+                  onChange={(e) =>
+                    setUser({ ...user, password: e.target.value })
+                  }
+                  placeholder="password"
+                />
+              </div>
+              <button
+                type="submit"
+                onClick={onLogin}
+                className="block w-full bg-indigo-600 mt-5 py-2 rounded-2xl hover:bg-indigo-700 hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-2"
+              >
+                Login
+              </button>
+              <div className="flex justify-between mt-4">
+                <span className="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all">
+                  Forgot Password ?
+                </span>
+
+                <Link
+                  href="/signup"
+                  className="text-sm ml-2 hover:text-blue-500 cursor-pointer hover:-translate-y-1 duration-500 transition-all"
+                >
+                  Don't have an account yet?
+                </Link>
+              </div>
+            </form>
           </div>
         </div>
       </div>
--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -4,9 +4,6 @@ import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import { toast } from "react-hot-toast";
-import Icon from "../../public/lost.png";
-import Image from "next/image";
-import { SiGnuprivacyguard } from "react-icons/si";
 
 export default function SignupPage() {
   const router = useRouter();
@@ -47,103 +44,143 @@ export default function SignupPage() {
 
   return (
     <>
-      <div className="container px-6 mx-auto">
-        <div className="flex flex-col text-center md:text-left md:flex-row h-screen justify-evenly md:items-center">
-          <div className="flex flex-col w-full">
-            <div>
-              <Image src={Icon} width={100} height={100} alt="icon" />
-            </div>
-            <h1 className="text-5xl text-gray-800 font-bold xs:text-xs">
+      <div className="relative min-h-screen signup-bg flex items-center justify-center  py-7 px-4 sm:px-6 lg:px-8 bg-gray-500 bg-no-repeat bg-cover  ">
+        <div className="absolute bg-black opacity-60 inset-0 z-0"></div>
+        <div className="max-w-md w-full space-y-8 p-10 bg-white rounded-xl z-10">
+          <div className="text-center">
+            <h2 className="mt-6 text-3xl font-bold text-gray-900">
               Simply sign up!
-            </h1>
-            <p className="w-8/12 pt-3 mx-auto md:mx-0 text-gray-500">
+            </h2>
+            <p className="mt-2 text-sm text-gray-600">
               Missing keys? Wallet? Register your lost items and browse found
               posts.
             </p>
           </div>
-          <div className="w-full md:w-full lg:w-9/12 mx-auto md:mx-0">
-            <div className="bg-white p-7 flex flex-col w-full shadow-xl rounded-xl items-center">
-              <h2 className="text-3xl md:text-3xl  font-bold text-gray-800 text-left mb-5">
-                Signup
-              </h2>
-              <form action="" className="w-full">
-                <div id="input" className="flex flex-col w-full my-2">
-                  <label for="username" className="text-gray-500 mb-2">
-                    Name
-                  </label>
-                  <input
-                    id="username"
-                    type="text"
-                    autoComplete="off"
-                    value={user.username}
-                    onChange={(e) =>
-                      setUser({ ...user, username: e.target.value })
-                    }
-                    placeholder="username"
-                    className="appearance-none border-2 text-black border-gray-100 rounded-lg px-4 py-3 placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:shadow-lg"
+
+          <form className="mt-8 space-y-4" action="#" method="POST">
+            <input type="hidden" name="remember" value="true" />
+            <div className="mt-8 content-center">
+              <label className="text-sm font-bold text-gray-700 tracking-wide">
+                Name
+              </label>
+              <div className="flex items-center">
+                {" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-gray-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    clipRule="evenodd"
                   />
-                </div>
-                <div id="input" className="flex flex-col w-full my-2">
-                  <label for="password" className="text-gray-500 mb-2">
-                    Email
-                  </label>
-                  <input
-                    id="email"
-                    type="email"
-                    autoComplete="off"
-                    value={user.email}
-                    onChange={(e) =>
-                      setUser({ ...user, email: e.target.value })
-                    }
-                    placeholder="email"
-                    className="appearance-none border-2 text-black border-gray-100 rounded-lg px-4 py-3 placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:shadow-lg"
-                  />
-                </div>
-                <div id="input" className="flex flex-col w-full my-2">
-                  <label for="password" className="text-gray-500 mb-2">
-                    Password
-                  </label>
-                  <input
-                    id="password"
-                    type="password"
-                    autoComplete="off"
-                    value={user.password}
-                    onChange={(e) =>
-                      setUser({ ...user, password: e.target.value })
-                    }
-                    placeholder="password"
-                    className="appearance-none border-2 text-black border-gray-100 rounded-lg px-4 py-3 placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600 focus:shadow-lg"
-                  />
-                </div>
-                <div id="button" className="flex flex-col w-full my-5">
-                  <button
-                    type="button"
-                    onClick={onSignup}
-                    className="w-full py-4 bg-green-600 rounded-lg text-green-100 hover:bg-green-800"
-                  >
-                    <div className="flex flex-row items-center justify-center">
-                      <div className="font-bold flex float-row gap-1 items-center">
-                        Signup
-                        <div className="mr-2 text-lg">
-                          {" "}
-                          <SiGnuprivacyguard />
-                        </div>
-                      </div>
-                      <div className="mr-2 text-xl"></div>
-                    </div>
-                  </button>
-                  <div className="flex justify-evenly mt-5">
-                    <Link
-                      href="/login"
-                      className="w-full text-center font-medium text-gray-500 "
-                    >
-                      Visit Login page
-                    </Link>
-                  </div>
-                </div>
-              </form>
+                </svg>
+                <input
+                  className="w-full text-black content-center text-base py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500"
+                  id="username"
+                  type="text"
+                  autoComplete="off"
+                  value={user.username}
+                  onChange={(e) =>
+                    setUser({ ...user, username: e.target.value })
+                  }
+                  placeholder="username"
+                />
+              </div>
             </div>
-          </div>
+            <div className="relative">
+              <label className="text-sm font-bold text-gray-700 tracking-wide">
+                Email
+              </label>
+              <div className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-gray-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                  />
+                </svg>
+                <input
+                  className=" w-full text-black text-base py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500"
+                  id="email"
+                  type="email"
+                  autoComplete="off"
+                  value={user.email}
+                  onChange={(e) => setUser({ ...user, email: e.target.value })}
+                  placeholder="email"
+                />
+              </div>
+            </div>
+            <div className="mt-8 content-center">
+              <label className="text-sm font-bold text-gray-700 tracking-wide">
+                Password
+              </label>
+              <div className="flex items-center">
+                {" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-gray-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                    clipRule="evenodd"
+                  />
+                </svg>{" "}
+                <input
+                  className="w-full text-black content-center text-base py-2 border-b border-gray-300 focus:outline-none focus:border-indigo-500"
+                  id="password"
+                  type="password"
+                  autoComplete="off"
+                  value={user.password}
+                  onChange={(e) =>
+                    setUser({ ...user, password: e.target.value })
+                  }
+                  placeholder="password"
+                />
+              </div>
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="text-sm items-center ">
+                <Link
+                  href="/"
+                  className="font-medium text-indigo-500 hover:text-indigo-500"
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+            </div>
+            <div>
+              <button
+                type="button"
+                onClick={onSignup}
+                className="w-full flex justify-center bg-indigo-500 text-gray-100 p-4  rounded-full tracking-wide
+                                font-semibold  focus:outline-none focus:shadow-outline hover:bg-indigo-600 shadow-lg hover:-translate-y-1 transition-all duration-500 cursor-pointer ease-in "
+              >
+                Sign up
+              </button>
+            </div>
+            <p className="flex flex-col items-center justify-center mt-10 text-center text-md text-gray-500">
+              <span>Already have an account?</span>
+              <Link
+                href="/login"
+                className="text-indigo-500 hover:text-indigo-500no-underline hover:underline hover:-translate-y-1 transition-all duration-500 cursor-pointer ease-in "
+              >
+                Login
+              </Link>
+            </p>
+          </form>
         </div>
       </div>
     </>
